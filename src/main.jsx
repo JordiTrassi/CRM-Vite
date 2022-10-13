223cb1da84a3c6ae5115dc8b9ae8788c59f64ba4
JordiTrassi <j.trassi@telefonica.net>
2022-10-14 00:07:08 +0200

    Installed React Router Dom and refacted files structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Inicio</h1>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
