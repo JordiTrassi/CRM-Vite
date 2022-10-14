039a9d4f91990b21b6c14203a30568b687c7889c
JordiTrassi <j.trassi@telefonica.net>
2022-10-14 13:05:25 +0200

    Added useLocation hook and dynamic styles in Layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,22 +1,31 @@
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet, Link, useLocation } from 'react-router-dom'
 
 export const Layout = () => {
-  return (
-    <div className='md:flex md:min-h-screen'>
-        <aside className='md:w-1/4 bg-blue-800 px-5 py-10'>
-              <h2 className='text-4xl font-black text-center text-white'>CRM Clientes</h2>
 
-              <nav className='mt-10'>
-                  <Link className='text-2xl block mt-2 hover:text-blue-300 text-white' to="/">Clientes</Link>
-                  <Link className='text-2xl block mt-2 hover:text-blue-300 text-white' to="/clientes/nuevo">Nuevo Cliente</Link>
-              </nav>
-        </aside>
-          
-        <main className='md:w-3/4 p-10 md:h-screen overflow-scroll'>
-            <Outlet />
-        </main>  
-    </div>
-  )
+    const location = useLocation();
+
+    return (
+        <div className='md:flex md:min-h-screen'>
+            <aside className='md:w-1/4 bg-blue-800 px-5 py-10'>
+                <h2 className='text-4xl font-black text-center text-white'>CRM Clientes</h2>
+
+                <nav className='mt-10'>
+                    <Link 
+                        className={`${location.pathname === '/' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`} 
+                        to="/"
+                    >Clientes</Link>
+                    <Link 
+                        className={`${location.pathname === '/clientes/nuevo' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`} 
+                        to="/clientes/nuevo"
+                    >Nuevo Cliente</Link>
+                </nav>
+            </aside>
+            
+            <main className='md:w-3/4 p-10 md:h-screen overflow-scroll'>
+                <Outlet />
+            </main>  
+        </div>
+    )
 }
 
 
