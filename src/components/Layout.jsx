498068757a1102b586dd5cac7cc63a94924a9ad8
JordiTrassi <j.trassi@telefonica.net>
2022-10-14 12:53:30 +0200

    Added Links in Layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,15 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom'
 
 export const Layout = () => {
   return (
     <div className='md:flex md:min-h-screen'>
         <aside className='md:w-1/4 bg-blue-800 px-5 py-10'>
               <h2 className='text-4xl font-black text-center text-white'>CRM Clientes</h2>
+
+              <nav className='mt-10'>
+                  <Link className='text-2xl block mt-2 hover:text-blue-300 text-white' to="/">Clientes</Link>
+                  <Link className='text-2xl block mt-2 hover:text-blue-300 text-white' to="/clientes/nuevo">Nuevo Cliente</Link>
+              </nav>
         </aside>
           
         <main className='md:w-3/4 p-10 md:h-screen overflow-scroll'>
